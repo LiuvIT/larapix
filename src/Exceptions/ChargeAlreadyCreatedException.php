093c9113288eaf5539f0d1b3ff342723e5ddb2fa
danielhe4rt <idanielreiss@gmail.com>
2022-03-17 17:27:44 -0300

    feat(charge): adding charges exceptions

---
+++ src/Exceptions/ChargeAlreadyCreatedException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Liuv\Larapix\Exceptions;
+
+use Exception;
+
+class ChargeAlreadyCreatedException extends Exception
+{
+
+}
---
+++ src/Exceptions/ChargeNotFoundException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Liuv\Larapix\Exceptions;
+
+class ChargeNotFoundException extends \Exception
+{
+
+}
