a27a8dd0ade878a35dc6bb5fff42d10e36d9433b
danielhe4rt <idanielreiss@gmail.com>
2022-03-18 16:27:05 -0300

    feat(refund): create refund value object

---
+++ src/ValueObjects/Refund.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Liuv\Larapix\ValueObjects;
+
+
+class Refund implements \JsonSerializable
+{
+    private $transactionEndToEndId;
+
+    private $correlationId;
+
+    private $value;
+
+    public function __construct(
+        string $correlationId, 
+        string $transactionEndToEndId, 
+        int $value
+    )
+    {
+        $this->corretionId = $correlationId;
+        $this->transactionEndToEndId = $transactionEndToEndId;
+        $this->value = $value;
+    }
+
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'correlationID' => $this->corretionId,
+            'transactionEndToEndId' => $this->transactionEndToEndId,
+            'value' => $this->value,
+        ];
+    }
+}
